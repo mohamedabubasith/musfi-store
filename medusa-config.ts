74c3123f21a25f83cba1948569ecbf8ed5c73980
mohamedabubasith <mohamedabu.basith@gmail.com>
2026-05-07 22:54:11 +0530

    removed the i created codes

--- medusa-config.ts
+++ medusa-config.ts
@@ -5,18 +5,12 @@ loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
-    redisUrl: process.env.REDIS_URL,
     http: {
       storeCors: process.env.STORE_CORS!,
       adminCors: process.env.ADMIN_CORS!,
       authCors: process.env.AUTH_CORS!,
-      jwtSecret: process.env.JWT_SECRET || 'supersecret',
-      cookieSecret: process.env.COOKIE_SECRET || 'supersecret',
-    },
-    workerMode: process.env.MEDUSA_WORKER_MODE as "shared" | "worker" | "server",
-  },
-  admin: {
-    disable: process.env.DISABLE_MEDUSA_ADMIN === 'true',
-    backendUrl: process.env.MEDUSA_BACKEND_URL,
-  },
+      jwtSecret: process.env.JWT_SECRET || "supersecret",
+      cookieSecret: process.env.COOKIE_SECRET || "supersecret",
+    }
+  }
 })
